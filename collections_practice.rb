2d6df945b93e7cf2f4e48d5254dd3971172fea2b
Kat B <katreesebarnes@gmail.com>
2019-03-19 23:55:17 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+require "pry"
 
+def sort_array_asc(array)
+  
+    array.sort do |num, num2|
+  
+  if num == num2
+    0 
+    elsif num < num2 
+    -1
+  else num > num2
+    1
+  end
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |num, num2|
+    if num == num2
+      0 
+      elsif num < num2
+      1 
+    else num > num2
+      -1
+    end 
+  end
+  
+end 
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.length}
+   
+   end
+   
+   def swap_elements(array)
+   array[0], array[1], array[2] = array[0], array[2], array[1]
+   
+   end
+   
+   def reverse_array(array)
+     array[0], array[1], array[2] = array[2], array[1], array[0]
+     
+   end
+   
+   def kesha_maker(array)
+     new_array =[]
+     array.each do |elements|
+       
+       elements[2] = '$'
+      new_array 
+    end
+  end   
+    
+   
