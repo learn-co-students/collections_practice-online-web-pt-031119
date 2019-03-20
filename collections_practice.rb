9783cd41071e74b7957bdfbd1f9b4b4fdb1d1988
MKanko <mkanko66@gmail.com>
2019-03-19 21:09:48 -0700

    completed sorting lab

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
+require 'pry'
 
+def sort_array_asc(numbers)
+  numbers.sort {|a, b| a <=> b}
+end 
+
+def sort_array_desc(numbers)
+  numbers.sort.reverse {|a, b| a <=> b}
+end 
+
+def sort_array_char_count(words)
+  words.sort {|a, b| a.length <=> b.length}
+end 
+
+def swap_elements(collection)
+  collection[1], collection[2] = collection[2], collection[1]
+  collection
+end  
+
+#Advanced Question 
+def swap_elements_from_to(array, index, dest_index)
+    array[index], array[dest_index] = array[dest_index], array[index]
+    array
+end
+
+def reverse_array(numbers)
+    numbers.reverse
+end  
+
+def kesha_maker(words)
+  kesha_words = []
+  words.each do |word|
+    kesha_words << word[2]= "$" 
+    kesha_words
+  end  
+end
+
+def find_a(words)
+  words.find_all {|word| word.start_with?("a")}
+end 
+
+# def sum_array(integers)
+#   integers.reduce(:+)
+# end 
+
+def sum_array(integers)
+  integers.inject {|sum, n| sum + n}
+end 
+
+def add_s(words)
+  words.each_with_index.collect do |element, index| 
+    if index != 1
+      "#{element}" << "s"
+    else
+      element = element   
+    end
+  end   
+end 
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question Advanced   
+  describe '#swap_elements_from_to' do
+    it 'swaps elements by specifying the index of the element to be moved to new index' do
+      expect(swap_elements_from_to(["a", "b", "c"], 0, 2)).to eq(["c", "b", "a"])
+    end 
+  end  
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
