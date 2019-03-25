59217b6ef694cab2e67f0056407739e2c30f3e90
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-25 02:50:54 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort{|x,y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort{|x| x.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  count = 0
+  kesharray[]
+  array.each {|a| do
+    kesharray[count] = (a[2] = "$")
+  end}
+  return kesharray
+end
